8e0668579106c2833e04e83bbe73325bd1795a49
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-11-17 22:46:10 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  createStore
+} from 'redux'
+
+function reducer(state, action) {
+
+  if (action.type === 'changeState')
+  {
+    return action.payload.newState;
+  }
+    console.log(action);
+  return 'State';
+}
+
+const store = createStore(reducer);
+console.log("tis rhe" + store.getState());
+const action = {
+  type: "changeState",
+  payload: {
+    newState: 'new State'
+  }
+}
+
+
+store.dispatch(action);
+console.log(store.getState());
+
+
+
+
+
+
+ReactDOM.render( < App / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
