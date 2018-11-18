e9265c8a31f5e50b1cffd2cdbef17ab8f98b1394
Shoaib Shaikh <shoaibshaikh1516@gmail.com>
2018-11-18 15:50:03 +0530

    combine Redux

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import {
-  createStore
-} from 'redux'
+import { combineReducers, createStore } from "redux";
 
-function reducer(state, action) {
-
-  if (action.type === 'changeState') {
-    return action.payload.newState;
-  }
-  console.log(action);
-  return 'State';
+function productsReducer(state = [], action) {
+  return state;
 }
 
-const store = createStore(reducer);
+function userReducer(state = "", action) {
+  switch (action.type) {
+    case "updateUser":
+      return action.payload;
+  }
+  return state;
+}
+
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+
+const store = createStore(
+  allReducers,
+  {
+    products: [{ name: "iPhone" }],
+    user: "Michael"
+  },
+  window.devToolsExtension && window.devToolsExtension()
+);
+
 console.log("tis rhe" + store.getState());
+
+
 const action = {
   type: "changeState",
   payload: {
-    newState: 'new State'
+    newState: "new State"
   }
-}
+};
 
 store.dispatch(action);
 console.log(store.getState());
 
+const updateUserAction = {
+  type: "updateUser",
+  payload: {
+    user: "john"
+  }
+};
+store.dispatch(updateUserAction);
+console.log(store.getState());
 
-
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
